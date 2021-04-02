cc11c509b356bcc1c0d8a116d46ab806e8df84bd
Arief Hidayatulloh <ariefhidayatulloh@gmail.com>
2021-04-02 18:27:30 +0700

    checkpoint implementation gorepo interface

--- gomysql/Gomysql.go
+++ gomysql/Gomysql.go
@@ -1,7 +1,20 @@
 package gomysql
 
+import "github.com/ariefsam/gorepo"
+
 type Gomysql struct {
 	Connection string
-	Database   string
 	PrimaryKey string
+}
+
+func (g Gomysql) Set(tablename string, id string, data interface{}) (err error) {
+	return
+}
+
+func (g Gomysql) Fetch(tablename string, filter *gorepo.Filter, data interface{}) (err error) {
+	return
+}
+
+func (g Gomysql) Delete(tablename string, id string) (err error) {
+	return
 }
